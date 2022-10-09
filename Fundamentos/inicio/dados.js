6ffd3ecd42742ead382b082c27ab549c664f6cf0
lannydev <e.cristina00@gmai.com>
2022-10-09 01:11:36 -0300

    trabalhando com dados 1

---
+++ Fundamentos/inicio/dados.js
@@ -0,0 +1,10 @@
+var quantidade = 10;
+var preco = 6.4;
+var valorTotal = quantidade * preco;
+
+console.log("Elaine");
+console.log(quantidade);
+console.log(preco);
+console.log(2 + 3);
+console.log(valorTotal);
+console.log(10);
