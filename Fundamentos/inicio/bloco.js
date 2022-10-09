c135bcc61e1b5075a8d28c555209d627caef3799
lannydev <e.cristina00@gmai.com>
2022-10-09 00:53:40 -0300

    bloco de código

---
+++ Fundamentos/inicio/bloco.js
@@ -0,0 +1,13 @@
+// bloco de código é composto por par de chaves
+//é possível criar blocos de códigos dentro de outro código
+
+{
+    console.log('sentença de código dentro do primeiro bloco');
+}
+
+{
+    console.log("primeira sentença do segundo bloco de código");
+    console.log("segunda sentença do segundo bloco de código");
+    console.log("terceira sentença do segundo bloco de código");
+    console.log("quarta sentença do segundo bloco de código");
+}
