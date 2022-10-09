604c90bdebbf90a044c362308e42df5cf8be5cfc
lannydev <e.cristina00@gmai.com>
2022-10-08 23:58:12 -0300

    aprendendo comentário

---
+++ Fundamentos/inicio/comentario.js
@@ -0,0 +1,11 @@
+console.log('passo 1');
+console.log('passo 2'); // após as duas barras o código não vai ser executado
+console.log('passo 3');
+
+/*
+este é um 
+comentário 
+de 
+multipĺas
+linhas!
+*/
