32f32ab1a301f45d13c35a0184edbcf92a493a96
lannydev <e.cristina00@gmai.com>
2022-10-09 20:22:03 -0300

    Desafio troca de valores.

---
+++ Fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 10;
+
+console.log(a);
+console.log(b);
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
--- Fundamentos/inicio/dsafio_area.js
+++
@@ -1,7 +0,0 @@
-// PI * raio *raio
-
-let raio = 15
-let area = Math.PI * (Math.pow(raio, 2));
-
-console.log("A área é: " + area);
-console.log(Math.pow(raio, 2));
