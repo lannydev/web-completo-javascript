abdfb4848bd5411362e4672e4f7bb43ac36c34c5
lannydev <e.cristina00@gmai.com>
2022-10-09 01:01:29 -0300

    primeiro desafio

--- Fundamentos/inicio/comentario.js
+++ Fundamentos/inicio/comentario.js
@@ -1,6 +1,6 @@
 console.log('passo 1');
 console.log('passo 2'); // após as duas barras o código não vai ser executado
-console.log('passo 3');
+// console.log('passo 3');
 
 /*
 este é um 
---
+++ Fundamentos/inicio/desafio1.js
@@ -0,0 +1,19 @@
+// ciar dois blocos de códigos
+// um bloco com 5 sentenças, onde cada sentença vai impromir numero impares de 1 a 10
+// o outro bloco com 5 sentenças, onde cada sentença vai impromir numero pares de 1 a 10
+
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
