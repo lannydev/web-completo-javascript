c3e17ee8a83ea51c90aebe7769b3312679fcd153
lannydev <e.cristina00@gmai.com>
2022-10-09 01:24:30 -0300

    Trabalhando com dados 2

--- Fundamentos/inicio/dados.js
+++ Fundamentos/inicio/dados.js
@@ -1,6 +1,6 @@
-var quantidade = 10;
+let quantidade = 10;
 var preco = 6.4;
-var valorTotal = quantidade * preco;
+let valorTotal = quantidade * preco;
 
 console.log("Elaine");
 console.log(quantidade);
@@ -8,3 +8,6 @@ console.log(preco);
 console.log(2 + 3);
 console.log(valorTotal);
 console.log(10);
+
+console.log("Elaine comprou " + quantidade + " canetas, cada uma custou " + preco + 
+    ", dando um total de " + valorTotal + ".");
